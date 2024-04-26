993cec64a8221c0ed0a39ab4ce5d72f59fcd8937
Милана <120707039+Dostoevskyfd@users.noreply.github.com>
2024-04-26 13:49:14 +0500

    medical college

---
+++ java/index.js
@@ -0,0 +1,220 @@
+/*слайд налево */
+function clic1(){
+    document.querySelector('.slaid_two').style.display = 'block';
+    document.querySelector('.right-btn_two').style.display = 'block';
+
+
+    document.querySelector('.slaid').style.display = 'none';
+    document.querySelector('.slaid_three').style.display = 'none';
+    document.querySelector('.slaid_four').style.display = 'none';
+    document.querySelector('.slaid_five').style.display = 'none';
+    document.querySelector('.right-btn').style.display = 'none';
+    document.querySelector('.right-btn_three').style.display = 'none';
+    document.querySelector('.right-btn_four').style.display = 'none';
+    document.querySelector('.right-btn_five').style.display = 'none';
+
+    
+    document.querySelector('.left-btn_five').style.display = 'block';
+
+    document.querySelector('.left-btn').style.display = 'none';
+    document.querySelector('.left-btn_four').style.display = 'none';
+    document.querySelector('.left-btn_two').style.display = 'none';
+    document.querySelector('.left-btn_three').style.display = 'none';
+}
+function clic2(){
+    document.querySelector('.slaid_three').style.display = 'block';
+    document.querySelector('.right-btn_three').style.display = 'block';
+
+
+    document.querySelector('.slaid').style.display = 'none';
+    document.querySelector('.slaid_two ').style.display = 'none';
+    document.querySelector('.slaid_four').style.display = 'none';
+    document.querySelector('.slaid_five').style.display = 'none';
+    document.querySelector('.right-btn').style.display = 'none';
+    document.querySelector('.right-btn_two').style.display = 'none';
+    document.querySelector('.right-btn_four').style.display = 'none';
+    document.querySelector('.right-btn_five').style.display = 'none';
+
+
+    document.querySelector('.left-btn_four ').style.display = 'block';
+
+    document.querySelector('.left-btn').style.display = 'none';
+    document.querySelector('.left-btn_five').style.display = 'none';
+    document.querySelector('.left-btn_two').style.display = 'none';
+    document.querySelector('.left-btn_three').style.display = 'none';
+}
+function clic3(){
+    document.querySelector('.slaid_four').style.display = 'block';
+    document.querySelector('.right-btn_four').style.display = 'block';
+
+
+    document.querySelector('.slaid').style.display = 'none';
+    document.querySelector('.slaid_two ').style.display = 'none';
+    document.querySelector('.slaid_three').style.display = 'none';
+    document.querySelector('.slaid_five').style.display = 'none';
+    document.querySelector('.right-btn').style.display = 'none';
+    document.querySelector('.right-btn_two').style.display = 'none';
+    document.querySelector('.right-btn_three').style.display = 'none';
+    document.querySelector('.right-btn_five').style.display = 'none';
+
+    
+    document.querySelector('.left-btn_three ').style.display = 'block';
+
+    document.querySelector('.left-btn').style.display = 'none';
+    document.querySelector('.left-btn_five').style.display = 'none';
+    document.querySelector('.left-btn_two').style.display = 'none';
+    document.querySelector('.left-btn_four').style.display = 'none';
+}
+
+function clic4(){
+    document.querySelector('.slaid_five').style.display = 'block';
+    document.querySelector('.right-btn_five').style.display = 'block';
+
+
+    document.querySelector('.slaid').style.display = 'none';
+    document.querySelector('.slaid_two ').style.display = 'none';
+    document.querySelector('.slaid_three').style.display = 'none';
+    document.querySelector('.slaid_four').style.display = 'none';
+    document.querySelector('.right-btn').style.display = 'none';
+    document.querySelector('.right-btn_two').style.display = 'none';
+    document.querySelector('.right-btn_three').style.display = 'none';
+    document.querySelector('.right-btn_four').style.display = 'none';
+
+    document.querySelector('.left-btn_two').style.display = 'block';
+
+    document.querySelector('.left-btn').style.display = 'none';
+    document.querySelector('.left-btn_five').style.display = 'none';
+    document.querySelector('.left-btn_five').style.display = 'none';
+    document.querySelector('.left-btn_three').style.display = 'none';
+    document.querySelector('.left-btn_four').style.display = 'none';
+}
+function clic5(){
+    document.querySelector('.slaid').style.display = 'block';
+    document.querySelector('.right-btn').style.display = 'block';
+
+
+    document.querySelector('.slaid_five').style.display = 'none';
+    document.querySelector('.slaid_two ').style.display = 'none';
+    document.querySelector('.slaid_three').style.display = 'none';
+    document.querySelector('.slaid_four').style.display = 'none';
+
+    document.querySelector('.right-btn_five').style.display = 'none';
+    document.querySelector('.right-btn_two').style.display = 'none';
+    document.querySelector('.right-btn_three').style.display = 'none';
+    document.querySelector('.right-btn_four').style.display = 'none';
+
+    document.querySelector('.left-btn').style.display = 'block';
+
+    document.querySelector('.left-btn_two').style.display = 'none';
+    document.querySelector('.left-btn_five').style.display = 'none';
+    document.querySelector('.left-btn_five').style.display = 'none';
+    document.querySelector('.left-btn_three').style.display = 'none';
+    document.querySelector('.left-btn_four').style.display = 'none';
+}
+
+/*слайд направо */
+function over1(){
+    document.querySelector('.right-btn_five').style.display = 'block';
+
+document.querySelector('.right-btn').style.display = 'none';
+    document.querySelector('.right-btn_two').style.display = 'none';
+    document.querySelector('.right-btn_three').style.display = 'none';
+    document.querySelector('.right-btn_four').style.display = 'none';
+
+    document.querySelector('.slaid_five ').style.display = 'block';
+    document.querySelector('.left-btn_two').style.display = 'block';
+
+    document.querySelector('.slaid').style.display = 'none';
+    document.querySelector('.slaid_three').style.display = 'none';
+    document.querySelector('.slaid_four').style.display = 'none';
+    document.querySelector('.slaid_two').style.display = 'none';
+
+    document.querySelector('.left-btn').style.display = 'none';
+    document.querySelector('.left-btn_five').style.display = 'none';
+    document.querySelector('.left-btn_three').style.display = 'none';
+    document.querySelector('.left-btn_four').style.display = 'none';
+   
+
+}
+function over2(){
+
+    document.querySelector('.right-btn_four').style.display = 'block';
+    document.querySelector('.slaid_four ').style.display = 'block';
+    document.querySelector('.left-btn_three').style.display = 'block';
+
+    document.querySelector('.slaid').style.display = 'none';
+    document.querySelector('.slaid_three').style.display = 'none';
+    document.querySelector('.slaid_five ').style.display = 'none';
+    document.querySelector('.slaid_two').style.display = 'none';
+
+    document.querySelector('.left-btn').style.display = 'none';
+    document.querySelector('.left-btn_five').style.display = 'none';
+    document.querySelector('.left-btn_two').style.display = 'none';
+    document.querySelector('.left-btn_four').style.display = 'none'; 
+
+    document.querySelector('.right-btn').style.display = 'none';
+    document.querySelector('.right-btn_two').style.display = 'none';
+    document.querySelector('.right-btn_three').style.display = 'none';
+    document.querySelector('.right-btn_five').style.display = 'none';
+}
+function over3(){
+
+    document.querySelector('.right-btn_three').style.display = 'block';
+    document.querySelector('.slaid_three ').style.display = 'block';
+    document.querySelector('.left-btn_four ').style.display = 'block';
+
+    document.querySelector('.slaid').style.display = 'none';
+    document.querySelector('.slaid_five').style.display = 'none';
+    document.querySelector('.slaid_four ').style.display = 'none';
+    document.querySelector('.slaid_two').style.display = 'none';
+
+    document.querySelector('.left-btn').style.display = 'none';
+    document.querySelector('.left-btn_five').style.display = 'none';
+    document.querySelector('.left-btn_three').style.display = 'none';
+    document.querySelector('.left-btn_two').style.display = 'none'; 
+
+    document.querySelector('.right-btn').style.display = 'none';
+    document.querySelector('.right-btn_two').style.display = 'none';
+    document.querySelector('.right-btn_four').style.display = 'none';
+    document.querySelector('.right-btn_five').style.display = 'none';
+}
+function over4(){
+    document.querySelector('.right-btn_two').style.display = 'block';
+    document.querySelector('.slaid_two ').style.display = 'block';
+    document.querySelector('.left-btn_five').style.display = 'block';
+
+    document.querySelector('.slaid').style.display = 'none';
+    document.querySelector('.slaid_five').style.display = 'none';
+    document.querySelector('.slaid_four ').style.display = 'none';
+    document.querySelector('.slaid_three ').style.display = 'none';
+
+    document.querySelector('.left-btn').style.display = 'none';
+    document.querySelector('.left-btn_four').style.display = 'none';
+    document.querySelector('.left-btn_three').style.display = 'none';
+    document.querySelector('.left-btn_two').style.display = 'none'; 
+
+    document.querySelector('.right-btn').style.display = 'none';
+    document.querySelector('.right-btn_three').style.display = 'none';
+    document.querySelector('.right-btn_four').style.display = 'none';
+    document.querySelector('.right-btn_five').style.display = 'none';
+}
+function over5(){
+    document.querySelector('.right-btn').style.display = 'block';
+    document.querySelector('.slaid').style.display = 'block';
+    document.querySelector('.left-btn').style.display = 'block';
+
+    document.querySelector('.slaid_two').style.display = 'none';
+    document.querySelector('.slaid_five').style.display = 'none';
+    document.querySelector('.slaid_four ').style.display = 'none';
+    document.querySelector('.slaid_three ').style.display = 'none';
+
+    document.querySelector('.left-btn_five').style.display = 'none';
+    document.querySelector('.left-btn_four').style.display = 'none';
+    document.querySelector('.left-btn_three').style.display = 'none';
+    document.querySelector('.left-btn_two').style.display = 'none';
+
+    document.querySelector('.right-btn_two').style.display = 'none';
+    document.querySelector('.right-btn_three').style.display = 'none';
+    document.querySelector('.right-btn_four').style.display = 'none';
+    document.querySelector('.right-btn_five').style.display = 'none';
+}
